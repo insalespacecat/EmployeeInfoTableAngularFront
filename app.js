6c33594eaecf7e6d21f516a2c235c9ee3ae62ea0
insalespacecat <60538056+insalespacecat@users.noreply.github.com>
2020-04-09 23:55:26 +0300

    Heroku server update patch

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const compression = require('compression');
+const app = express();
+
+app.use(compression());
+app.use(express.static(path.join(__dirname, 'dist/EmployeeInfoTable')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/EmployeeInfoTable/index.html'));
+});
+
+const port = process.env.PORT || 8080;
+app.set("port", port);
+const server = http.createServer(app);
+server.listen(port, () => console.log('server started ${ port }'));
