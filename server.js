2b275e87a9a80338a2306044e7e3332edafe3abb
insalespacecat <60538056+insalespacecat@users.noreply.github.com>
2020-04-10 00:14:16 +0300

    Patch for Heroku
    removed unsupported compression

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
-const compression = require('compression');
 const app = express();
 
-app.use(compression());
 app.use(express.static(path.join(__dirname, 'dist/EmployeeInfoTable')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/EmployeeInfoTable/index.html'));
